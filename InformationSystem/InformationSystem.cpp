5826cbb0fdc75deefb4c0616dc86e6243eb6f395
Timok Alekseev <timofey.alekseev@list.ru>
2024-04-29 22:26:32 +0300

    Добавьте файлы проекта.

---
+++ InformationSystem/InformationSystem.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class MPT {
+
+
+
+    private:class Group {
+        string name;
+    };
+
+    private:class Student {
+        Group owngroup;
+        vector<string> disciplines;
+        int 
+
+    };
+    
+    private:class Teacher{
+
+    };
+
+};
+
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
